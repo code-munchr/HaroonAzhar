34da65bc684a9d5fc10139ef682d5a609568ad4f
MacBook <macbook@MacBooks-MacBook-Pro.local>
2020-04-03 14:01:58 +0500

    basic  set up

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,33 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+
+require("./js/jquery-3.2.1.min.js")
+require("./js/popper.js")
+require("./js/bootstrap.min.js")
+require("./js/stellar.js")
+require("./vendors/lightbox/simpleLightbox.min.js")
+require("./vendors/nice-select/js/jquery.nice-select.min.js")
+require("./vendors/isotope/imagesloaded.pkgd.min.js")
+require("./vendors/isotope/isotope-min.js")
+require("./vendors/owl-carousel/owl.carousel.min.js")
+require("./js/jquery.ajaxchimp.min.js")
+require("./vendors/counter-up/jquery.waypoints.min.js")
+require("./vendors/counter-up/jquery.counterup.js")
+require("./js/mail-script.js")
+require("./js/theme.js")
+
+import 'bootstrap'
+import './src/application.scss'
+// Uncomment to copy all static images under ../images to the output folder and reference
+// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
+// or the `imagePath` JavaScript helper below.
+//
+// const images = require.context('../images', true)
+// const imagePath = (name) => images(name, true)
---
+++ db/migrate/20200328074325_create_items.rb
@@ -0,0 +1,13 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.integer :price
+      t.string :name
+      t.integer :quantity
+      t.string :description
+      t.references :category, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
